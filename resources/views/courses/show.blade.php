dced4c06355f5f3985a097b79712a0be54848ddd
JaimeC-code <centurionjaime@gmail.com>
2022-09-27 10:36:39 -0500

    progress of courses, contacts and reminder

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -7,6 +7,25 @@
 @endsection
 
 @section('content')
+    <div class="accordion" id="accordionExample">
+       
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse"
+                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Collapsible Group Item #2
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                    Some placeholder content for the second accordion panel. This panel is hidden by default.
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 @endsection
 
@@ -17,4 +36,3 @@
 @section('js')
 
 @endsection
-
